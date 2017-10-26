ab874780a4a7b621cb89d75f40a64c25d70f3a83
eerotpt <eero.sakson@tptlive.ee>
2017-10-26 21:33:34 +0200

    for_foreach

---
+++ 01_hello_world/excuses/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excuses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var excuses = new[] { "screensaver võlus", "12345", "külmkapp oli väike", "kõik korras" };
+
+            for (int i = 0; i < excuses.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine(excuses[i]);
+            }
+            Console.ReadLine();
+            
+                foreach (var wordInArray in excuses) 
+            { 
+                Console.WriteLine(wordInArray);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
