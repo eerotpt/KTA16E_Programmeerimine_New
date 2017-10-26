9f94bb0c8f63511232881a2795aeacfdf0bf6233
eerotpt <eero.sakson@tptlive.ee>
2017-10-26 10:03:31 +0200

    variables_entry

---
+++ 01_hello_world/Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            Console.WriteLine("Say and i will repeat");
+            Console.Write("You say: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("I say : " + input);
+            Console.ReadLine();
+
+
+        }
+    }
+}
