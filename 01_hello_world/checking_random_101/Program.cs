a09bea36d3b88c7781113b9cce26610e2a60bad0
eerotpt <eero.sakson@tptlive.ee>
2017-10-26 20:01:12 +0200

    check_random

---
+++ 01_hello_world/checking_random_101/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checking_random_101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //check random 0-100
+            while (true)
+            {   
+                int magic_Number = new Random().Next(1,101);// 0-100
+                Console.Write(" " + magic_Number);
+                System.Threading.Thread.Sleep(magic_Number+101);
+                if (magic_Number == 101)
+                {
+                    Console.ReadLine();
+                    break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ 01_hello_world/random_while/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace random_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string entry;
+            int magic_Number = new Random().Next(101);
+            Console.Write("Quess a number from 1 to 99: ");
+
+            while (true)
+            {
+
+                entry = Console.ReadLine();
+                int entry_int = int.Parse(entry);
+                if (entry_int == magic_Number)
+                {
+                    Console.WriteLine("Right " +" "+ entry_int +" "+ entry);
+                    Console.ReadLine();
+                    break;
+                }
+                else if (entry_int < magic_Number)
+                {
+                    Console.WriteLine("number is bigger ");
+                }
+                else
+                {
+                    Console.WriteLine("number is smaller");
+                }
+
+            }
+                    Console.ReadLine();
+                
+                
+            }                
+    }
+}
